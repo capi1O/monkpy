1c2127148d5e753b3d2ab0af1b7a03e1e2ac2b50
monkeydri <monkeydri@github.com>
2017-07-07 18:20:01 +0200

    create initial module  structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='monkpy',
+      version='0.1',
+      description='handy python functions',
+      url='http://github.com/monkeydri/monkpy',
+      author='monkeydri',
+      author_email='monkeydri@github.com',
+      license='MIT',
+      packages=['monkpy'],
+      zip_safe=False)
